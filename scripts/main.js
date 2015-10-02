a0136adceafe971a4366facc08de58c87ab40743
Marcus <Binarius1100@gmail.com>
2015-10-02 13:42:21 +1000

    Reset

--- scripts/main.js
+++ scripts/main.js
@@ -6,10 +6,33 @@ var context = canvas.getContext("2d");
 context.mozImageSmoothingEnabled = false;
 context.imageSmoothingEnabled = false;
 
+var gameState = 0;
+
+// Button stuff
+var inversion = new Image();
+inversion.src = "images/misc/inversion.png";
+
+//unselected
+var quit_us = document.createElement("img");
+quit_us.src = "images/misc/quit.png";
+
+var start_us = document.createElement("img");
+start_us.src = "images/misc/start.png";
+
+//draws images
+function startMenuButtons()
+{	
+	context.drawImage(inversion, 540, 200, 508, 124);
+    context.drawImage(start_us, 700, 340, 200, 50);
+    context.drawImage(quit_us, 700, 400, 200, 50);
+}
+
 // Make the main game variables
 var keyboard = new Keyboard();
-var player = new Player(256, 256);
+var sound = new Sound();
 var world = new World(48, 48);
+var player = new Player(world.levelOrigins[world.currentLevel], world.levelOrigins[world.currentLevel + 1]);
+var effects = [];
 
 // Set up frame counting (FPS)
 var frameCounter = 0;
@@ -18,6 +41,9 @@ var frames = 0;
 // Camera variables
 var cameraX = player.X;
 var cameraY = player.Y;
+
+var previousLeft = false;
+var previousRight = false;
 
 // More camera variables
 var displacementX = (canvas.width / 2) - (player.width / 2);
@@ -33,59 +59,125 @@ function clear()
 	context.fillRect(0, 0, canvas.width, canvas.height);
 }
 
+// Mouse
+var mouse = new Mouse();
+
 // Function: Main game loop
 function run()
 {
-	// Every time the game draws a frame, increase the FPS by 1
-	frameCounter++;
+    // Every time the game draws a frame, increase the FPS by 1
+    frameCounter++;
 	
-	// Clear the screen
-	clear();
+    // Clear the screen
+    clear();
 	
-	// Update Logic
-	// Update the world
-	world.update();
-		
-	// Update the player with the keyboard
-	player.update(world.XG,
-			world.YG,
-			keyboard.isKeyDown(keyboard.KEY_LEFT),
-			keyboard.isKeyDown(keyboard.KEY_RIGHT),
-			keyboard.isKeyDown(keyboard.KEY_SPACE),
-			world.levels[world.currentLevel]);
-	
-	// Drawing Logic
-	// Move Camera
-	cameraX += (player.X - cameraX) / 5;
-	cameraY += (player.Y - cameraY) / 5;
-		
-	// Draw the world
-	world.draw(displacementX, displacementY, cameraX, cameraY);
-	
-	// Draw the player
-	player.draw(displacementX,
-			displacementY,
-			cameraX,
-			cameraY,
-			(player.X - cameraX) * 2,
-			(player.Y - cameraY) * 2,
-			keyboard.isKeyDown(keyboard.KEY_LEFT),
-			keyboard.isKeyDown(keyboard.KEY_RIGHT));
+    if (gameState == 0)
+    {
+        // Menu
+        /*
+		example_button.update();
+		example_button1.update();
+		example_button.draw();
+		example_button1.draw();
+        */
 
-	// Draw the FPS counter
-	context.fillStyle = "#111";
+		startMenuButtons();
+    }
+    else if (gameState == 1)
+    {
+        // Game
+
+        if (!player.isDead)
+        {
+            // Update Logic
+            // Update the world
+            world.update();
+
+            // Update the player with the keyboard
+            player.update(world.XG,
+                    world.YG,
+                    keyboard.isKeyDown(keyboard.KEY_LEFT),
+                    keyboard.isKeyDown(keyboard.KEY_RIGHT),
+                    previousLeft, previousRight,
+                    keyboard.isKeyDown(keyboard.KEY_SPACE));
+
+            previousLeft = keyboard.isKeyDown(keyboard.KEY_LEFT);
+            previousRight = keyboard.isKeyDown(keyboard.KEY_RIGHT);
+        }
+        else
+        {
+            window.setTimeout(respawn, 1000);
+        }
+
+        // Update effects
+        for (var i = 0; i < effects.length; i++)
+        {
+            effects[i].update();
+            if (effects[i].tileIndex / effects[i].tickTime > effects[i].tiles)
+                effects.splice(i, 1);
+        }
+
+        // Drawing Logic
+        // Move Camera
+        cameraX += (player.X - cameraX) / 5;
+        cameraY += (player.Y - cameraY) / 5;
+
+        // Draw the world
+        world.draw(displacementX, displacementY, cameraX, cameraY);
+
+        // Draw effects
+        for (var i = 0; i < effects.length; i++)
+            effects[i].draw(displacementX, displacementY, cameraX, cameraY);
+
+        if (!player.isDead)
+        {
+            // Draw the player
+            player.draw(displacementX,
+                    displacementY,
+                    cameraX,
+                    cameraY,
+                    (player.X - cameraX) * 2,
+                    (player.Y - cameraY) * 2,
+                    keyboard.isKeyDown(keyboard.KEY_LEFT),
+                    keyboard.isKeyDown(keyboard.KEY_RIGHT),
+                    keyboard.isKeyDown(keyboard.KEY_SPACE));
+        }
+
+        // Draw the fail counter
+        context.font = "55px Trebuchet MS";
+        context.fillStyle = "#000";
+        context.fillText(world.fails + " FAILS", 30, 850);
+        context.fillStyle = "#FFF";
+        context.fillText(world.fails + " FAILS", 35, 845)
+    }
+    else if (gameState == 2)
+    {
+        // Credits/You win
+    }
+
+    // Draw the FPS counter
 	context.font = "12px Courier";
-	context.fillText(frames + " FPS", 10, 15);
-	
+	context.fillStyle = "#FFF";
+	context.fillText("Inversion - Alpha 0.5 - " + frames + " FPS - Alpaca Trousers", 10, 15);
+	context.fillText("Cert II IDMT Project", 10, 30);
+	context.fillText("Game State: " + gameState, 10, 45);
+
 	// Make sure the next frame draws
 	requestAnimationFrame(run);
+}
+
+function respawn()
+{
+    this.player = new Player(world.levelOrigins[2 * world.currentLevel], world.levelOrigins[(2 * world.currentLevel) + 1]);
+
+    this.world.reset();
 }
 
 // Frame Counter (FPS)
 setInterval(function()
 {
 	frames = frameCounter;
-	console.log(frameCounter + " fps");
+	//console.log(frameCounter + " fps");
 	frameCounter = 0;
 }, 1000);
 
---
+++ scripts/sounds.js
@@ -0,0 +1,93 @@
+var Sound = function()
+{
+    // Create all the sound effects for the game manually
+    this.deathSound = new Howl
+    ({
+        urls: ["sounds/death.wav"],
+        volume: 1.0,
+        autoplay: false,
+        buffer: true
+    });
+    this.enterSound = new Howl
+    ({
+        urls: ["sounds/enter.wav"],
+        volume: 1.0,
+        autoplay: false,
+        buffer: true
+    });
+    this.fallSound = new Howl
+    ({
+        urls: ["sounds/fall.wav"],
+        volume: 1.0,
+        autoplay: false,
+        buffer: true
+    });
+    this.longFallSound = new Howl
+    ({
+        urls: ["sounds/longfall.wav"],
+        volume: 1.0,
+        autoplay: false,
+        buffer: true
+    });
+    this.screamSound = new Howl
+    ({
+        urls: ["sounds/scream.wav"],
+        volume: 1.0,
+        autoplay: false,
+        buffer: true
+    });
+    this.selectSound = new Howl
+    ({
+        urls: ["sounds/select.wav"],
+        volume: 1.0,
+        autoplay: false,
+        buffer: true
+    });
+    this.shiftSound = new Howl
+    ({
+        urls: ["sounds/shift.wav"],
+        volume: 1.0,
+        autoplay: false,
+        buffer: true
+    });
+
+    this.stepSounds = [];
+    this.stepSounds[0] = new Howl
+    ({
+        urls: ["sounds/step_0.wav"],
+        volume: 1.0,
+        autoplay: false,
+        buffer: true
+    });
+    this.stepSounds[1] = new Howl
+    ({
+        urls: ["sounds/step_1.wav"],
+        volume: 1.0,
+        autoplay: false,
+        buffer: true
+    });
+
+    this.menuMusic = new Howl
+    ({
+        urls: ["sounds/music/menu.mp3"],
+        volume: 1.0,
+        autoplay: false,
+        buffer: true,
+        fadein: true,
+        looped: true
+    });
+
+    // Set up step sound index
+    this.stepSoundIndex = 0;
+    this.stepSoundIndexMax = this.stepSounds.length;
+}
+
+Sound.prototype.playStepSound = function()
+{
+    this.stepSounds[this.stepSoundIndex].play();
+
+    this.stepSoundIndex++;
+    if (!(this.stepSoundIndex < this.stepSoundIndexMax)) this.stepSoundIndex = 0;
+
+    console.log("New step sound index: " + this.stepSoundIndex);
+}
